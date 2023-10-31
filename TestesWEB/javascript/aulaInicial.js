f86cca40103f8a1912f2d6810ea5047b69f5ce67
Carlos Alexandre <carlosalexandre1431.caf@gmail.com>
2023-10-30 20:18:43 -0400

    AulaJavascript

---
+++ TestesWEB/javascript/aulaInicial.js
@@ -0,0 +1,55 @@
+
+// function fn2(){
+//     setTimeout(() => {
+//         console.log('oi'), 3000;
+//     });
+// }
+
+// function fn1(){
+//     fn2();
+// }
+
+// function main(){
+//     fn1();
+// }
+
+// main();
+// console.log("Tchau mundo");
+
+// var nome = 'Lívia';
+// console.log(nome);
+
+// nome = 22;
+// console.log(nome);
+
+// console.log(nome + 10)
+
+// console.log(true);
+// console.log(false);
+
+// var valor1 = 'VALOR1';
+
+// function main(){
+//     var valor2 = "Valor2";
+//     console.log(valor1);
+//     console.log(valor2);
+// }
+
+// console.log(valor2);
+
+// main();
+
+const euclido = 1;
+
+
+function main(){
+    // if(euclido > 2){
+    //     console.log('TA ERRADO');
+    // }
+    // else if(euclido == 1){
+    //     console.log('TA CERTO');
+    // }
+
+}
+
+main();
